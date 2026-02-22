529d6371c7a92067ae9c20e46390d2b9f0128687
RileyGDev <rileygweb@gmail.com>
2026-02-21 20:35:09 -0700

    Mobile stuff

---
+++ android/app/src/main/java/org/memorizeyourscripture/www/MainActivity.java
@@ -0,0 +1,5 @@
+package org.memorizeyourscripture.www;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,14 @@
 import './bootstrap';
-import './pwa';
+import './pwa';import { App } from '@capacitor/app';
+
+// Detect if running in Capacitor app and add class for styling
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const info = await App.getInfo();
+        if (info) {
+            document.documentElement.classList.add('capacitor-app');
+        }
+    } catch (error) {
+        // Not running in Capacitor, continue normally
+    }
+});
